4c60c391466046d4ed62081744b418a4441f1a49
sahil kharb <sahil.kharb@go-jek.com>
2016-07-17 23:34:46 +0530

    operation subtraction is implemented

--- Operation.hs
+++ Operation.hs
@@ -1,3 +1,5 @@
 module Operation where
 
 add number1 number2 = number1 + number2
+
+subtract number1 number2 = number1 - number2
--- OperationSpec.hs
+++ OperationSpec.hs
@@ -8,3 +8,6 @@ main = hspec $ do
   describe "add" $ do
     it "should return 5 when addition is performed on 3 and 2" $
         add 1 2 `shouldBe` 3
+  describe "subtraction" $ do
+    it "should return 1 when subtraction is performed on 3 and 2" $
+        Operation.subtract 3 2 `shouldBe` 1
