6f2bccdb79c8d044eeb3c60cca8b6029686e170a
sahil kharb <sahil.kharb@go-jek.com>
2016-07-18 00:18:44 +0530

    Implement division operator  * Last spec of division operator is hack, will fix next time

--- Operation.hs
+++ Operation.hs
@@ -5,3 +5,5 @@ add number1 number2 = number1 + number2
 subtract number1 number2 = number1 - number2
 
 multiply number1 number2 = number1 * number2
+
+divide number1 number2 = number1 / number2
--- OperationSpec.hs
+++ OperationSpec.hs
@@ -14,3 +14,10 @@ main = hspec $ do
   describe "multiplication" $ do
     it "should return 6 when multiplication is performed on 3 and 2" $
         multiply 3 2 `shouldBe` 6
+    it "should return 0 when either of operand is zero" $
+        multiply 2 0 `shouldBe` 0
+  describe "division" $ do
+    it "should return 1.5 when division is performed on 3 and 2" $
+        divide 3 2 `shouldBe` 1.5
+    it "should return Infinity when denominator is 0" $
+        divide 3 0 `shouldBe` 1 / 0
