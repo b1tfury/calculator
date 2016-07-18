a9b8a2f478b62031c4473f2caea60e7ca2a08020
sahil kharb <sahilkharb@iitj.ac.in>
2016-07-18 19:22:31 +0530

    modularize the operation functions and  replace its use cases

--- Operation.hs
+++ Operation.hs
@@ -1,9 +1,11 @@
 module Operation where
 
-add number1 number2 = number1 + number2
+operation operator operand1 operand2 = operator operand1 operand2
 
-subtract number1 number2 = number1 - number2
+add number1 number2 = operation (+) number1 number2
 
-multiply number1 number2 = number1 * number2
+subtract number1 number2 = operation (-) number1 number2
 
-divide number1 number2 = number1 / number2
+multiply number1 number2 = operation (*) number1 number2
+
+divide number1 number2 = operation (/) number1 number2
